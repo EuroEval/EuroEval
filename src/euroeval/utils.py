056f5352b89b310254965f41ee471eb277e9d1e4
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-04-05 09:25:26 +0200

    fix: Clean first tokens, not local labels

--- src/euroeval/utils.py
+++ src/euroeval/utils.py
@@ -630,15 +630,18 @@ def get_first_label_token_mapping(
             labels_to_be_generated=local_labels, tokenizer=tokenizer
         ):
             local_labels = [f" {label}" for label in local_labels]
-        local_labels = [
-            re.sub(
-                pattern=r"^[^a-zæøåüöä]+|[^a-zæøåüöä]+$", repl="", string=label.lower()
-            )
-            for label in local_labels
-        ]
 
         # Get the first token of each label
         first_tokens = [tokenizer.tokenize(text=label)[0] for label in local_labels]
+        first_tokens = [
+            re.sub(
+                pattern=r"^[^a-zæøåüöä]+|[^a-zæøåüöä]+$", repl="", string=token.lower()
+            )
+            for token in first_tokens
+        ]
+
+        # Build a mapping from labels to the first token in each label if the first
+        # tokens are distinct
         if len(first_tokens) == len(set(first_tokens)):
             log_once(
                 "The model will output scores, since the first tokens of the labels "
