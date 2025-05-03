92435bf12af4d08141453dd392e6746fc9d91e7d
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-05-03 12:22:29 +0200

    debug

--- src/euroeval/finetuning.py
+++ src/euroeval/finetuning.py
@@ -226,6 +226,7 @@ def finetune_single_iteration(
         trainer.add_callback(NeverLeaveProgressCallback)
 
     try:
+        breakpoint()
         trainer.train()
         with torch.inference_mode():
             try:
