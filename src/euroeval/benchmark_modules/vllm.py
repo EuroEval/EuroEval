54bff8a0f3bfec2f2990db2633fdd31e3cd41533
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-06-03 21:06:55 +0200

    debug

--- src/euroeval/benchmark_modules/vllm.py
+++ src/euroeval/benchmark_modules/vllm.py
@@ -1088,6 +1088,7 @@ def get_end_of_reasoning_token(
             "This is probably not correct, so please report this issue.",
             level=logging.INFO,
         )
+        breakpoint()
         return None
 
     if len(eor_reasoning_matches) > 1:
