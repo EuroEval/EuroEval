ad2a6c002a773732571151e9ded48c99cefb5109
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-06-16 15:11:40 +0200

    fix: Divide eval_accumulation_steps by batch_size

--- src/euroeval/finetuning.py
+++ src/euroeval/finetuning.py
@@ -297,7 +297,7 @@ def get_training_args(
         save_total_limit=1,
         per_device_train_batch_size=batch_size,
         per_device_eval_batch_size=batch_size,
-        eval_accumulation_steps=32,
+        eval_accumulation_steps=32 // batch_size,
         optim=OptimizerNames.ADAMW_TORCH,
         learning_rate=2e-5,
         warmup_ratio=0.01,
