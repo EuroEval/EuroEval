3ad88df327fb363326b101fdb173ef93e14bd054
Oliver Kinch <oliver.kinch@gmail.com>
2025-06-24 16:09:34 +0200

    Change dataset_id as for dataset config

--- src/scripts/create_norwegian_idioms.py
+++ src/scripts/create_norwegian_idioms.py
@@ -89,7 +89,7 @@ def main() -> None:
     )
 
     # Create dataset ID
-    dataset_id = "EuroEval/norwegian-idioms"
+    dataset_id = "EuroEval/idioms-no"
 
     # Remove the dataset from Hugging Face Hub if it already exists
     try:
