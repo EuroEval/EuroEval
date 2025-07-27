4662430daab7313dc2febb5798019aa0c7dd1adb
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-07-27 15:16:14 +0200

    fix: Do not delete loaded_model after model eval

--- src/euroeval/benchmarker.py
+++ src/euroeval/benchmarker.py
@@ -495,7 +495,7 @@ class Benchmarker:
                     f"{total_benchmarks} benchmarks."
                 )
 
-            del loaded_model
+            # del loaded_model
             if benchmark_config.clear_model_cache:
                 clear_model_cache_fn(cache_dir=benchmark_config.cache_dir)
 
