ca47dcc2d52ddf924e4cd7cff54b911416bb0e1d
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-07-30 18:15:01 +0200

    chore: Remove all other dataset configs, as we only want MultiWikiQA

--- src/euroeval/dataset_configs/multiwikiqa.py
+++ src/euroeval/dataset_configs/multiwikiqa.py
@@ -1,26 +1,23 @@
 """Template for dataset configs for new languages."""
 
-from huggingface_hub import HfApi
-
 from .. import languages
 from ..data_models import DatasetConfig
 from ..tasks import RC
 
-globals().update(
-    {
-        f"MULTI_WIKI_QA_{language.upper()}_CONFIG": DatasetConfig(
-            name=f"multi-wiki-qa-{language}",
-            pretty_name=f"the truncated version of the {language} part of the reading "
-            "comprehension dataset MultiWikiQA",
-            huggingface_id=f"EuroEval/multi-wiki-qa-{language}-mini",
-            task=RC,
-            languages=[getattr(languages, language.upper())],
+for language, cfg in languages.get_all_languages().items():
+    try:
+        globals().update(
+            {
+                f"MULTI_WIKI_QA_{language.upper()}_CONFIG": DatasetConfig(
+                    name=f"multi-wiki-qa-{language}",
+                    pretty_name=f"the truncated version of the {cfg.name} part of the "
+                    "reading comprehension dataset MultiWikiQA",
+                    huggingface_id=f"EuroEval/multi-wiki-qa-{language}-mini",
+                    task=RC,
+                    languages=[getattr(languages, language.upper())],
+                )
+            }
         )
-        for language in [
-            cfg["config_name"].split(".")[-1]
-            for cfg in HfApi()
-            .repo_info(repo_id="alexandrainst/multi-wiki-qa", repo_type="dataset")
-            .card_data.configs
-        ]
-    }
-)
+    except AttributeError:
+        # If the language is not supported, we skip it
+        pass
