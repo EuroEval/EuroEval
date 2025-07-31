dd0fd96ac1bd2974fd2f02cd1d44ae3037c417db
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-07-31 10:37:44 +0200

    fix: Only add the dataset configs whose datasets exist

--- src/euroeval/dataset_configs/multiwikiqa.py
+++ src/euroeval/dataset_configs/multiwikiqa.py
@@ -1,23 +1,27 @@
 """Template for dataset configs for new languages."""
 
+from huggingface_hub import HfApi
+
 from .. import languages
 from ..data_models import DatasetConfig
 from ..tasks import RC
 
+api = HfApi()
 for language, cfg in languages.get_all_languages().items():
-    try:
-        globals().update(
-            {
-                f"MULTI_WIKI_QA_{language.upper()}_CONFIG": DatasetConfig(
-                    name=f"multi-wiki-qa-{language}",
-                    pretty_name=f"the truncated version of the {cfg.name} part of the "
-                    "reading comprehension dataset MultiWikiQA",
-                    huggingface_id=f"EuroEval/multi-wiki-qa-{language}-mini",
-                    task=RC,
-                    languages=[getattr(languages, language.upper())],
-                )
-            }
-        )
-    except AttributeError:
-        # If the language is not supported, we skip it
-        pass
+    # Skip if the language is not supported by MultiWikiQA
+    if not api.repo_exists(f"EuroEval/multi-wiki-qa-{language}-mini"):
+        continue
+
+    # Create the dataset config
+    dataset_config = DatasetConfig(
+        name=f"multi-wiki-qa-{language}",
+        pretty_name=f"the truncated version of the {cfg.name} part of the "
+        "reading comprehension dataset MultiWikiQA",
+        huggingface_id=f"EuroEval/multi-wiki-qa-{language}-mini",
+        task=RC,
+        languages=[getattr(languages, language.upper())],
+        _num_few_shot_examples=2,
+    )
+
+    # Add the config to the globals
+    globals().update({f"MULTI_WIKI_QA_{language.upper()}_CONFIG": dataset_config})
