d68d6699fd0fc4c986ac0b02eaed26357449080a
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-09-01 21:15:00 +0200

    chore: Remove all `ray` mentions

--- src/euroeval/benchmark_modules/vllm.py
+++ src/euroeval/benchmark_modules/vllm.py
@@ -81,9 +81,6 @@ if t.TYPE_CHECKING or importlib.util.find_spec("vllm") is not None:
     from vllm.lora.request import LoRARequest
     from vllm.sampling_params import GuidedDecodingParams
 
-if t.TYPE_CHECKING or importlib.util.find_spec("ray") is not None:
-    import ray
-
 if t.TYPE_CHECKING:
     from datasets import DatasetDict
     from transformers.tokenization_utils import PreTrainedTokenizer
@@ -120,10 +117,7 @@ class VLLMModel(HuggingFaceEncoderModel):
             log_metadata:
                 Whether to log the model and dataset metadata.
         """
-        if (
-            importlib.util.find_spec("vllm") is None
-            or importlib.util.find_spec("ray") is None
-        ):
+        if importlib.util.find_spec("vllm") is None:
             raise NeedsExtraInstalled(extra="generative")
 
         model, tokeniser = load_model_and_tokeniser(
@@ -955,12 +949,8 @@ def clear_vllm() -> None:
     with contextlib.suppress(ValueError):
         destroy_model_parallel()
         destroy_distributed_environment()
-    if ray.is_initialized():
-        ray.shutdown()
     with contextlib.suppress(AssertionError):
         torch.distributed.destroy_process_group()
-    if ray.is_initialized():
-        ray.shutdown()
     clear_memory()
 
 
