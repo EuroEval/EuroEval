4339a274718e7a0de705aa1f0e6cda386d4add13
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-09-01 11:41:51 +0200

    debug: Try not blocking outputs

--- src/euroeval/__init__.py
+++ src/euroeval/__init__.py
@@ -12,11 +12,11 @@ import warnings
 from termcolor import colored
 
 # Block specific warnings before importing anything else, as they can be noisy
-warnings.filterwarnings("ignore", category=UserWarning)
-logging.getLogger("httpx").setLevel(logging.CRITICAL)
-logging.getLogger("datasets").setLevel(logging.CRITICAL)
-logging.getLogger("vllm").setLevel(logging.CRITICAL)
-os.environ["VLLM_CONFIGURE_LOGGING"] = "0"
+# warnings.filterwarnings("ignore", category=UserWarning)
+# logging.getLogger("httpx").setLevel(logging.CRITICAL)
+# logging.getLogger("datasets").setLevel(logging.CRITICAL)
+# logging.getLogger("vllm").setLevel(logging.CRITICAL)
+# os.environ["VLLM_CONFIGURE_LOGGING"] = "0"
 
 # Set up logging
 # fmt = colored("%(asctime)s", "light_blue") + " ⋅ " + colored("%(message)s", "green")
@@ -60,7 +60,7 @@ from .utils import block_terminal_output  # noqa: E402
 # Block unwanted terminal outputs. This blocks way more than the above, but since it
 # relies on importing from the `utils` module, external modules are already imported
 # before this is run, necessitating the above block as well
-block_terminal_output()
+# block_terminal_output()
 
 
 # Fetches the version of the package as defined in pyproject.toml
