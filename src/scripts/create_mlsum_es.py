31f0232292e64556bdc6422d1fcd70369c8073be
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-09-08 21:37:41 +0200

    style: Use missing_ok=True in creation scripts before pushing to hub

--- src/scripts/create_mlsum_es.py
+++ src/scripts/create_mlsum_es.py
@@ -14,7 +14,6 @@ import pandas as pd
 from constants import MAX_NUM_CHARS_IN_ARTICLE, MIN_NUM_CHARS_IN_ARTICLE
 from datasets import Dataset, DatasetDict, Split, load_dataset
 from huggingface_hub import HfApi
-from requests import HTTPError
 
 
 def main() -> None:
@@ -67,14 +66,10 @@ def main() -> None:
     )
 
     # Create dataset ID
-    mini_dataset_id = "ScandEval/mlsum-es-mini"
+    mini_dataset_id = "EuroEval/mlsum-es-mini"
 
     # Remove the dataset from Hugging Face Hub if it already exists
-    try:
-        api: HfApi = HfApi()
-        api.delete_repo(mini_dataset_id, repo_type="dataset")
-    except HTTPError:
-        pass
+    HfApi().delete_repo(dataset_id, repo_type="dataset", missing_ok=True)
 
     # Push the dataset to the Hugging Face Hub
     dataset.push_to_hub(mini_dataset_id, private=True)
