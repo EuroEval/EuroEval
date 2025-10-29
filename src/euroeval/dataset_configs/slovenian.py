698c102d42c922c5cbf6305ed3e544d61c9f2379
Oliver Kinch <oliver.kinch@gmail.com>
2025-10-29 14:17:17 +0100

    Add Slovenian SA dataset Sentinews

---
+++ src/euroeval/dataset_configs/slovenian.py
@@ -0,0 +1,47 @@
+"""All Slovenian dataset configurations used in EuroEval."""
+
+from ..data_models import DatasetConfig
+from ..languages import SLOVENIAN
+from ..tasks import COMMON_SENSE, KNOW, LA, RC, SENT
+
+### Official datasets ###
+
+CSFD_SENTIMENT_SK_CONFIG = DatasetConfig(
+    name="sentinews",
+    pretty_name="Sentinews-sl",
+    source="EuroEval/sentinews-mini",
+    task=SENT,
+    languages=[SLOVENIAN],
+)
+
+SCALA_SK_CONFIG = DatasetConfig(
+    name="scala-sk",
+    pretty_name="ScaLA-sk",
+    source="EuroEval/scala-sk",
+    task=LA,
+    languages=[SLOVENIAN],
+)
+
+MULTI_WIKI_QA_SK_CONFIG = DatasetConfig(
+    name="multi-wiki-qa-sk",
+    pretty_name="MultiWikiQA-sk",
+    source="EuroEval/multi-wiki-qa-sk-mini",
+    task=RC,
+    languages=[SLOVENIAN],
+)
+
+MMLU_SK_CONFIG = DatasetConfig(
+    name="mmlu-sk",
+    pretty_name="MMLU-sk",
+    source="EuroEval/mmlu-sk-mini",
+    task=KNOW,
+    languages=[SLOVENIAN],
+)
+
+WINOGRANDE_SK_CONFIG = DatasetConfig(
+    name="winogrande-sk",
+    pretty_name="Winogrande-sk",
+    source="EuroEval/winogrande-sk",
+    task=COMMON_SENSE,
+    languages=[SLOVENIAN],
+)
