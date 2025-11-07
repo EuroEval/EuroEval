68a4aae35e08713d84b7b1afaace4e3b97771d5b
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-11-07 10:01:09 +0100

    fix: Avoid negative stride/max_length in RC

--- src/euroeval/task_group_utils/question_answering.py
+++ src/euroeval/task_group_utils/question_answering.py
@@ -427,9 +427,13 @@ def prepare_test_examples(
     max_question_tokens = max(len(tokeniser(q).input_ids) for q in examples["question"])
     num_special_tokens = int(has_cls_token) + int(has_sep_token)
     stride = tokeniser.model_max_length // 4
+    stride = min(
+        stride,
+        tokeniser.model_max_length - stride - max_question_tokens - num_special_tokens,
+    )
+    stride = max(stride, 0)
     max_length = tokeniser.model_max_length - stride
-    stride = min(stride, max_length - max_question_tokens - num_special_tokens)
-    max_length = tokeniser.model_max_length - stride
+    max_length = max(max_length, 0)
 
     # Tokenise our examples with truncation and maybe padding, but keep the overflows
     # using a stride. This results in one example possible giving several features when
