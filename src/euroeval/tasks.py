2a870b90011aabb3ca88f88a20ccf331ef573434
Collin Aldaibis <caldabis@gmail.com>
2025-12-22 15:11:09 +0100

    move multiple-choice-stereotype-bias to a more fitting section

--- src/euroeval/tasks.py
+++ src/euroeval/tasks.py
@@ -142,6 +142,30 @@ EUROPEAN_VALUES = Task(
 )
 
 
+MCSTEREO = Task(
+    name="multiple-choice-stereotype-bias",
+    task_group=TaskGroup.MULTIPLE_CHOICE_CLASSIFICATION,
+    template_dict=MULTIPLE_CHOICE_TEMPLATES,
+    metrics=[
+        m.bias_adjusted_accuracy_ambig_metric,
+        m.bias_adjusted_accuracy_disambig_metric,
+        m.bias_ambig_metric,
+        m.bias_disambig_metric,
+        m.accuracy_ambig_metric,
+        m.accuracy_disambig_metric,
+    ],
+    default_num_few_shot_examples=0,
+    default_max_generated_tokens=NUM_GENERATION_TOKENS_FOR_CLASSIFICATION,
+    default_labels=["a", "b", "c"],
+    default_allowed_model_types=[ModelType.GENERATIVE],
+    default_allowed_generative_types=[
+        GenerativeType.INSTRUCTION_TUNED,
+        GenerativeType.REASONING,
+    ],
+    uses_logprobs=True,
+)
+
+
 SPEED = Task(
     name="speed",
     task_group=TaskGroup.SPEED,
@@ -186,30 +210,6 @@ MULTIPLE_CHOICE = Task(
     default_max_generated_tokens=NUM_GENERATION_TOKENS_FOR_CLASSIFICATION,
     default_labels=None,
     default_allowed_model_types=[ModelType.GENERATIVE],
-    uses_logprobs=True,
-)
-
-
-MCSTEREO = Task(
-    name="multiple-choice-stereotype-bias",
-    task_group=TaskGroup.MULTIPLE_CHOICE_CLASSIFICATION,
-    template_dict=MULTIPLE_CHOICE_TEMPLATES,
-    metrics=[
-        m.bias_adjusted_accuracy_ambig_metric,
-        m.bias_adjusted_accuracy_disambig_metric,
-        m.bias_ambig_metric,
-        m.bias_disambig_metric,
-        m.accuracy_ambig_metric,
-        m.accuracy_disambig_metric,
-    ],
-    default_num_few_shot_examples=0,
-    default_max_generated_tokens=NUM_GENERATION_TOKENS_FOR_CLASSIFICATION,
-    default_labels=["a", "b", "c"],
-    default_allowed_model_types=[ModelType.GENERATIVE],
-    default_allowed_generative_types=[
-        GenerativeType.INSTRUCTION_TUNED,
-        GenerativeType.REASONING,
-    ],
     uses_logprobs=True,
 )
 
