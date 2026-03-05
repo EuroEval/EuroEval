53a32c66041f8e556684ef13ccc5ead4bd0713de
saattrupdan <saattrupdan@gmail.com>
2026-03-05 13:54:09 +0100

    fix: Use absolute imports in scripts

--- src/scripts/create_nordial.py
+++ src/scripts/create_nordial.py
@@ -12,10 +12,9 @@
 
 import pandas as pd
 import requests
+from constants import MAX_NUM_CHARS_IN_DOCUMENT, MIN_NUM_CHARS_IN_DOCUMENT  # noqa
 from datasets import Dataset, DatasetDict, Split
 from huggingface_hub import HfApi
-
-from .constants import MAX_NUM_CHARS_IN_DOCUMENT, MIN_NUM_CHARS_IN_DOCUMENT  # noqa
 
 BASE_URL = (
     "https://raw.githubusercontent.com/jerbarnes/nordial/refs/heads/main/"
